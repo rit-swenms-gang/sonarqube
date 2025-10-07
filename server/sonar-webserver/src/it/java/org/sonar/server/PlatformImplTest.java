4de6999d8df9a5563293b54f575dc2c59f75f43b
Christian Ashley <christianzashley@gmail.com>
2025-10-06 22:49:21 -0400

    Add progress; failing from larger env complexity

---
+++ server/sonar-webserver/src/it/java/org/sonar/server/PlatformImplTest.java
@@ -0,0 +1,57 @@
+package org.sonar.server;
+
+import static org.mockito.Mockito.mock;
+
+import java.util.Properties;
+
+import org.junit.After;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.DisableOnDebug;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestRule;
+import org.junit.rules.Timeout;
+import org.sonar.api.config.internal.MapSettings;
+import org.sonar.api.testfixtures.log.LogTester;
+import org.sonar.api.utils.System2;
+import org.sonar.server.platform.PlatformImpl;
+import org.sonar.server.platform.db.EmbeddedDatabase;
+
+import jakarta.servlet.ServletContext;
+
+public class PlatformImplTest {
+  @Rule
+  public LogTester logTester = new LogTester();
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+  @Rule
+  public TestRule safeguardTimeout = new DisableOnDebug(Timeout.seconds(60));
+
+  private MapSettings settings = new MapSettings();
+  private System2 system2 = mock(System2.class);
+  private EmbeddedDatabase testDb = new EmbeddedDatabase(settings.asConfig(), system2);
+
+  @After
+  public void tearDown() {
+    if (testDb != null) {
+      testDb.stop();
+    }
+  }
+
+  @Ignore
+  @Test
+  public void test_doStart() {
+    Properties mock_props = mock(Properties.class);
+    ServletContext mock_ctx = mock(ServletContext.class);
+    PlatformImpl testPI = PlatformImpl.getInstance();
+    
+    //problems: unable to mock database instance before able to access doStart
+    testDb.start();
+    testPI.init(mock_props, mock_ctx);
+
+    // testPI.doStart();
+
+    // assertThat(mock_ctx.getServletRegistrations()).hasSize(1);
+  }
+}
