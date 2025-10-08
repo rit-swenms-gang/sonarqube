43bd0ce6a15a27b674d8b0967670a1ff71ee5061
Andrew <70240081+ABradbury823@users.noreply.github.com>
2025-10-07 21:58:20 -0400

    add default issue comment tests

---
+++ sonar-core/src/test/java/org/sonar/core/issue/DefaultIssueCommentTest.java
@@ -0,0 +1,54 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.core.issue;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DefaultIssueCommentTest {
+  @Test
+  public void create_comment_without_user_uuid() {
+    DefaultIssueComment comment = DefaultIssueComment.create("issueKey", "markdown text");
+    assertEquals("markdown text", comment.markdownText());
+    assertEquals("issueKey", comment.issueKey());
+    assertNull(comment.userUuid());
+    assertNotNull(comment.key());
+    assertNotNull(comment.createdAt());
+    assertNotNull(comment.updatedAt());
+    assertTrue(comment.isNew());
+  }
+
+  @Test
+  public void create_comment_with_user_uuid() {
+    DefaultIssueComment comment = DefaultIssueComment.create("issueKey", "userUuid", "markdown text");
+    assertEquals("markdown text", comment.markdownText());
+    assertEquals("issueKey", comment.issueKey());
+    assertEquals("userUuid", comment.userUuid());
+    assertNotNull(comment.key());
+    assertNotNull(comment.createdAt());
+    assertNotNull(comment.updatedAt());
+    assertTrue(comment.isNew());
+  }
+
+}
